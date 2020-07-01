4376afc31d431c82639eea7efb26199a38658ec9
Arthur Xavier <Arthxvr@outlook.com>
2020-07-01 19:12:04 -0300

    Add files to upload
    Add some"CodingBat" python exercises solutions of list Warmup-1
    diff21, front_back, missing_char, monkey_trouble, near_hundred, parrot_trouble, pos_neg, sleep_in, sum_double

---
+++ CodingBat/diff21.py
@@ -0,0 +1,3 @@
+def diff21(n):
+    return abs(n - 21) if n < 21 else 2 * abs(n - 21)
+    
---
+++ CodingBat/pos_neg.py
@@ -0,0 +1,3 @@
+def pos_neg(a, b, negative):
+   return (a < 0) and (b < 0) if negative else a * b < 0
+    
