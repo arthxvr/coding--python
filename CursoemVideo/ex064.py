a8134ff5327dce52ecf2a0fe4128c4eaf1f684c0
Arthur Xavier <arthxvr@outlook.com>
2020-11-17 14:19:33 -0300

    Atualiza exercício ex064.py

--- CursoemVideo/ex064.py
+++ CursoemVideo/ex064.py
@@ -1,10 +1,8 @@
 n = cont = soma = 0
-limite = False
+n = int(input('Digite um valor[999 para parar]: '))
 while not limite:
     soma += n
     cont += 1
     n = int(input('Digite um valor[999 para parar]: '))
-    if n == 999:
-        limite = True
 print(f'Foram digitados: {cont} números.')
 print(f'A soma entre eles é: {soma}')
